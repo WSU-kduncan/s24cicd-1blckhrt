43459e1c303094c23deb02108b96b938a5ae27d8
1blckhrt <williams.1691@wright.edu>
2024-04-18 09:39:12 -0400

    fixing deploy script for testing video

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -7,7 +7,7 @@ sudo docker remove nginx
 
 # pull fresh image
 
-sudo docker pull 1blckhrt/dockerhubrepo:1.2.1
+sudo docker pull 1blckhrt/dockerhubrepo:2.0.1
 
 # run new image
-sudo docker run -d -p 80:80 --name nginx --restart always 1blckhrt/dockerhubrepo:1.2.1
+sudo docker run -d -p 80:80 --name nginx --restart always 1blckhrt/dockerhubrepo:2.0.1
